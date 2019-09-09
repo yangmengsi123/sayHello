5c6cdc7ae9deaac9ccec5e0b9f520ffa3116b655
Yang <Yang@126.com>
2019-09-09 17:19:40 +0800

    create a sayHello class

---
+++ SayHello.java
@@ -0,0 +1,13 @@
+public class SayHello{
+   public static void main(String[] args){
+	   System.out.println("Below is the function of sayhello");
+	   sayHello(0);
+   }
+    public static void sayHello(int sum){
+		System.out.println("say Hello!"+sum);
+	}
+	public static int sum(){
+		return 1+3;
+	}
+}
+    
